6f5318e06c84bf15abd32ba6fe5ac6abcb956f91
Carolyn Walcott <carolynoliveira515@gmail.com>
2017-12-21 01:39:27 -0500

    Checkpoint 5: User Roles - Completed

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -18,7 +18,7 @@ class WikisController < ApplicationController
 
     if @wiki.save
       flash[:notice] = "Wiki was saved."
-      redirect_to @wiki
+      redirect_to wikis_path
     else
       flash.now[:alert] = "There was an error saving the wiki. Please try again."
       render :new
@@ -36,7 +36,7 @@ class WikisController < ApplicationController
 
     if @wiki.save
       flash[:notice] = "Wiki was updated."
-      redirect_to @wiki
+      redirect_to wikis_path
     else
       flash.now[:alert] = "There was an error saving the wiki. Please try again."
       render :edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis, dependent: :destroy
+
+  before_save { self.email = email.downcase }
+  before_save { self.role ||= :standard }
+
+  enum role: [:standard, :premium, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :standard
+  end
 end
