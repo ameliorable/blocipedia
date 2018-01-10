978145bd7e8db23a899e0b172a7de60275f671fe
Carolyn Walcott <carolynoliveira515@gmail.com>
2018-01-10 13:07:42 -0500

    Checkpoint 8: Private Wikis - Completed

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -3,7 +3,9 @@ class DowngradeController < ApplicationController
   end
 
   def create
+    @wikis = current_user.wikis
     current_user.update_attribute(:role, 'standard')
+    @wikis.unscoped.update_all(private: 'false')
 
     flash[:notice] = "Aw, you have successfully downgraded your account! Please, check us out again."
 
